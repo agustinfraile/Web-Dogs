397dcce52f048e17ecfcb7aa155bf352a1a97fce
Agustin Fraile <agustinfraile98@gmail.com>
2023-03-08 10:20:20 -0300

    agregando estilos al detalle de cada card

--- client/src/pages/DogDetail/DogDetail.jsx
+++ client/src/pages/DogDetail/DogDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getDogDetail } from '../../redux/actions';
+import { Icon } from '@iconify/react';
 
 import './DogDetail.css';
 
@@ -17,33 +18,68 @@ const DogDetail = () => {
 
     return (
         <>
-            <div>
-                <h1>{dogDetail.name}</h1>
-                <img src={dogDetail.image} alt={dogDetail.name} />
-                <h3>{dogDetail.weight}kg</h3>
-                <h3>{dogDetail.height}cm</h3>
-                <h3>
-                    {
-                        dogDetail.createdInDb 
-                        ?
-                        dogDetail.temperaments
-                        ? dogDetail.temperament.name.join(" | ")
-                        : ""
-                        :
-                        dogDetail.temperament
-                        ? dogDetail.temperament.join(" | ")
-                        : "" 
-                        
-                    }
-                </h3>
-                <h4>id:{dogDetail.id}</h4>
+            <div className='detail'>
+                <div className='detail-image'>
+                    <img 
+                        src={dogDetail.image} 
+                        alt={dogDetail.name} 
+                    />
+                </div>
+
+                <div className='detail-container'>
+                        <div className='detail-container_title'>
+                            <h1>{dogDetail.name}</h1>
+                        </div>
+                    <div className='detail-container--info'>
+
+                        <div className='detail-container_weight'>
+                            <h3>
+                                <Icon icon="mdi:weight-kilogram" />
+                                {dogDetail.weight}kg
+                            </h3>
+                        </div>
+
+                        <div className='detail-container_height'>
+                            <h3>
+                                <Icon icon="gg:format-line-height" />
+                                {dogDetail.height}cm
+                            </h3>
+                        </div>
+
+                        <div 
+                            className='detail-container_temperament'
+                        >
+                            <h3>
+                            <Icon icon="bxs:dog" />
+                                {
+                                    dogDetail.createdInDb 
+                                    ?
+                                    dogDetail.temperaments
+                                    ? dogDetail.temperament.name.join(" | ")
+                                    : ""
+                                    :
+                                    dogDetail.temperament
+                                    ? dogDetail.temperament.join(" | ")
+                                    : "" 
+                                    
+                                }
+                            </h3>
+                        </div>
+
+                        {/* <div className='detail-container_id'>
+                            <h4>id:{dogDetail.id}</h4>
+                        </div> */}
+                    </div>
+                </div>
             </div>
 
-            <button
-              onClick={() => window.history.back()}
-            >
-              Volver
-            </button>
+            <div className='button-back'>
+                <button
+                onClick={() => window.history.back()}
+                >
+                Volver
+                </button>
+            </div>
         </>
     )
 }
