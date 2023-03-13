0e52d30cb716d9ee2268a845bb6327d0482e4e0b
Agustin Fraile <agustinfraile98@gmail.com>
2023-03-13 04:53:16 -0300

    agregando cambios al deploy

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors =require("cors");
+
 
 require('./db.js');
 
@@ -10,8 +12,13 @@ const server = express();
 
 server.name = 'API';
 
+var corsOptions= {
+  origin: ["http://localhost:3000"],
+}
+
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
+server.use(cors(corsOptions))
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
